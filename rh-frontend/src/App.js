363b860967e5c84677a8ae7991385b9d4f91ca80
Rick_Belson <belsonrj@aol.com>
2020-11-05 02:05:23 -0500

    second

--- rh-frontend/src/App.js
+++ rh-frontend/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/users')
+    .then(response => response.json)
+    .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div className="App">
