52e76343e1f07b8a466d18a62a4cae157be35791
Rick_Belson <belsonrj@aol.com>
2020-11-07 19:28:59 -0500

    setup store

--- rh-frontend/src/App.js
+++ rh-frontend/src/App.js
@@ -3,8 +3,14 @@ import React from 'react';
 class App extends React.Component {
 
   componentDidMount() {
-    fetch('http://localhost:3000/api/v1/users')
-    .then(response => response.json)
+    fetch('http://localhost:3001/api/v1/users', {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+
+    })
+    .then(res => res.json())
     .then(data => console.log(data))
   }
 
